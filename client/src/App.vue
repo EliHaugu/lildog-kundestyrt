e3d681c3ce7d3ea66323bab8cb67cc1516073020
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-09-04 13:47:25 +0200

    Add colour variables

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,6 @@
 
 <template>
   <main class="flex w-screen">
-    <h1 class="mx-auto text-2xl font-semibold">Hello World!</h1>
+    <h1 class="mx-auto text-2xl font-semibold text-accent-900">Hello World!</h1>
   </main>
 </template>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,25 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    colors: {},
+    colors: {
+      white: '#ffffff',
+      accent: {
+        400: '#BDEEED',
+        500: '#469796',
+        600: '#005958',
+        700: '#014848',
+        800: '#00312F',
+        900: '#001E1E'
+      },
+      disabled: {
+        500: '#B7B7B7',
+        600: '#E0E0E0'
+      },
+      error: {
+        500: '#E76C6C',
+        600: '#C53030'
+      }
+    },
     extend: {}
   },
   plugins: []
