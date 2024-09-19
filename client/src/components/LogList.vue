ebcf5d48043a81dbde98e4a64625cae6828b626b
EliHaugu <elin.haugum@hotmail.com>
2024-09-19 13:29:07 +0200

    Test failed pipeline

--- client/src/components/LogList.vue
+++ client/src/components/LogList.vue
@@ -4,14 +4,15 @@ defineProps<{
 }>()
 
 import { logItems } from '@/assets/mock_data'
+import { listItems } from '@/assets/mock_data'
 </script>
 <template>
-  <section class="mb-8 h-full max-h-[calc(100vh-14rem)] overflow-y-scroll">
+        <section class="mb-8 h-full max-h-[calc(100vh-14rem)] overflow-y-scroll">
     <ul v-if="deviceId !== 0">
       <li v-for="log in logItems.find((item) => item.id === deviceId)?.log" :key="log">
         {{ log }}
       </li>
-    </ul>
+          </ul>
     <ul v-else>
       <li
         :key="deviceLogItem"
