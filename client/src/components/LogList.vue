29a2359e25562245365ebfdec4af3a6d375c3d3b
Elin <elin.haugum@hotmail.com>
2024-09-21 17:51:53 +0200

    fixup! Set up initial client-server communication

--- client/src/components/LogList.vue
+++ client/src/components/LogList.vue
@@ -3,27 +3,25 @@ defineProps<{
   deviceId: number
 }>()
 
-import { defineProps, reactive, onMounted, onUnmounted } from 'vue';
-import { webSocketService } from '@/services/WebSocketService';
+import { defineProps, reactive, onMounted, onUnmounted } from 'vue'
+import { webSocketService } from '@/services/WebSocketService'
 
 const state = reactive({
-  logs: [] as { id: number, log: string }[]
-});
+  logs: [] as { id: number; log: string }[]
+})
 
-const handleNewLog = (log: { id: number, log: string }) => {
-  state.logs.push(log);
-};
+const handleNewLog = (log: { id: number; log: string }) => {
+  state.logs.push(log)
+}
 
 onMounted(() => {
-  webSocketService.connect(8765); // TODO: Should be called when test is run (but there is no such function yet)
-  webSocketService.subscribe(handleNewLog); 
-});
-
+  webSocketService.connect(8765) // TODO: Should be called when test is run (but there is no such function yet)
+  webSocketService.subscribe(handleNewLog)
+})
 
 onUnmounted(() => {
-  webSocketService.unsubscribe(handleNewLog);
-});
-
+  webSocketService.unsubscribe(handleNewLog)
+})
 </script>
 <template>
   <section class="mb-8 h-full max-h-[calc(100vh-14rem)] overflow-y-scroll">
@@ -33,10 +31,7 @@ onUnmounted(() => {
       </li>
     </ul>
     <ul v-else>
-      <li
-        :key="deviceLogItem.log"
-        v-for="deviceLogItem in state.logs"
-      >
+      <li :key="deviceLogItem.log" v-for="deviceLogItem in state.logs">
         {{ deviceLogItem.log }}
       </li>
     </ul>
