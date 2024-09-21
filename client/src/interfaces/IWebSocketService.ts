28ce9fd236cadb8d4634758f3fe19b003c4cc792
Adele Felicia Ellingsen-Grønningsæther <adelefeliciaeg@gmail.com>
2024-09-21 18:15:51 +0200

    Set up initial client-server communication (#18)
    * Set up initial client-server communication
    
    Co-authored-by: haakon98 <haakon98@users.noreply.github.com>
    
    * Add format check for main branch (#17)
    
    Add frontend format check for main branch
    
    ---------
    
    Co-authored-by: haakon98 <haakon98@users.noreply.github.com>
    Co-authored-by: Elin <elin.haugum@hotmail.com>

---
+++ client/src/interfaces/IWebSocketService.ts
@@ -0,0 +1,8 @@
+import type { Log } from '@/types/WebSocketServiceTypes'
+
+export interface IWebSocketService {
+  connect(port: number): void
+  subscribe(listener: (log: Log) => void): void
+  unsubscribe(listener: (log: Log) => void): void
+  notifyListeners(log: Log): void
+}
