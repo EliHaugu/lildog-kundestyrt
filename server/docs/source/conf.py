321dc9019fa944dbfca3101cf50759263a160bcc
Elin <elin.haugum@hotmail.com>
2024-09-23 23:32:30 +0200

    Draft docs

---
+++ server/docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Lildog kundestyrt'
+copyright = '2024, Timon Alexander Selnes, Jacob Benjamin Alveberg, Elin Haugum, Adele Felicia Ellingsen-Grønningsæther, Haakon Karstensen, Olav Selnes Lorentzen'
+author = 'Timon Alexander Selnes, Jacob Benjamin Alveberg, Elin Haugum, Adele Felicia Ellingsen-Grønningsæther, Haakon Karstensen, Olav Selnes Lorentzen'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
