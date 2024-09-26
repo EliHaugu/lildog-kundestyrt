5e705c58132736a7e91883b373415d092178c601
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-09-26 09:11:39 +0200

    Run formatting.

--- client/src/views/NewDeviceView.vue
+++ client/src/views/NewDeviceView.vue
@@ -23,7 +23,7 @@ const testConnection = ref(false)
         <input
           type="text"
           placeholder="Device name"
-          class="rounded-lg border border-accent-600 px-4 py-2 bg-primary-200"
+          class="rounded-lg border border-accent-600 bg-primary-200 px-4 py-2"
         />
         <div>
           <label for="newConnectionType">Connection Type:</label>
@@ -61,7 +61,7 @@ const testConnection = ref(false)
           </select>
           <button
             type="submit"
-            class="text-bg-900 rounded-xl dark:bg-accent-600 bg-accent-400 px-4 py-2 shadow-md hover:bg-accent-500"
+            class="text-bg-900 rounded-xl bg-accent-400 px-4 py-2 shadow-md hover:bg-accent-500 dark:bg-accent-600"
           >
             Add to devices
           </button>
