9114d13fbf46c68efab0bb275111e632b4a1273e
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-09-30 13:45:12 +0200

    Redesign flow nodes

---
+++ client/src/components/node/FieldInput.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  colour: String,
+  label: String,
+  value: String
+})
+
+defineEmits(['edit'])
+</script>
+
+<template>
+  <span class="flex items-center gap-1">
+    <label :for="label" class="text-md dark:text-white-100">{{ label }}:</label>
+    <input
+      :name="label"
+      type="text"
+      :value="value"
+      :style="{ backgroundColor: colour }"
+      class="ml-auto rounded-sm px-2 py-1"
+      @input="$emit('edit')"
+    />
+  </span>
+</template>
--- client/src/types/NodeType.ts
+++ client/src/types/NodeType.ts
@@ -4,6 +4,9 @@ export type CustomData = {
   label: string
   connection?: string
   flowId?: string
+  type?: string
+  testState: string
+  fields?: { uuid?: string; action?: string; assertion?: string }
 }
 
 export type CustomNode = Node<CustomData>
