53bcb066549f29b51dd47eb668999b42c890cf57
Haakon Karstensen <haakonkarstesen@dhcp-10-220-230-90.wlan.ntnu.no>
2024-10-17 14:02:58 +0200

    Format type: ignore

--- server/server_comm/server_comm/consumers.py
+++ server/server_comm/server_comm/consumers.py
@@ -1,6 +1,6 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer # type: ignore
+from channels.generic.websocket import WebsocketConsumer  # type: ignore
 
 
 class LogConsumer(WebsocketConsumer):
