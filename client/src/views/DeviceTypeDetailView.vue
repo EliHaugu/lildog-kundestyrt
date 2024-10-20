33a26d88bb5e5cb68bb80d8031dee0ce53199db4
olavsl <olav.selnes.lorentzen@hotmail.com>
2024-10-20 15:15:44 +0200

    finish device type detail view

--- client/src/views/DeviceTypeDetailView.vue
+++ client/src/views/DeviceTypeDetailView.vue
@@ -3,26 +3,31 @@ import type { DeviceType, Device } from '@/types/DeviceTypes'
 import { computed, inject, ref, type Ref } from 'vue';
 import { useRoute } from 'vue-router';
 import BaseButton from '@/components/BaseButton.vue';
+import BaseInputField from '@/components/BaseInputField.vue';
 import DeviceInstance from '@/components/devices/DeviceInstance.vue';
 import PlusIcon from '@/icons/PlusIcon.vue';
 
 const route = useRoute()
 const deviceTypeName = computed(() => route.params.deviceTypeName as string)
-console.log(deviceTypeName)
 const deviceTypes = inject<Ref<DeviceType[]>>('deviceTypes', ref([]))
-const updateDeviceTypes = inject<Ref<DeviceType[]>>('updateDeviceTypes', ref([]))
+const updateDeviceTypes = inject<(newDeviceTypes: DeviceType[]) => void>(
+  'updateDeviceTypes',
+  () => {}
+)
+const deviceType = deviceTypes.value.find((type) => type.name === deviceTypeName.value)
 
 const addNewDevice = ref<boolean>(false)
 const newDeviceName = ref<string>('')
-
-const editedDevice = ref<Device | null>(null)
-const editDeviceName = ref<string>('')
-
-const deviceType = deviceTypes.value.find((type) => type.name === deviceTypeName.value) 
+const editDevice = ref<Device | null>(null)
+const showEditDeviceForm = ref<boolean>(false)
 
 function handleAddNewDevice() {
+    let newDeviceId: number;
+    do {
+        newDeviceId = Math.floor(100 + Math.random() * 900);
+    } while (deviceType?.devices?.some(device => device.id === newDeviceId));
     const newDevice: Device = {
-        id: deviceType?.devices?.length ? deviceType.devices.length + 1 : 1,
+        id: newDeviceId,
         name: newDeviceName.value,
         deviceType: deviceType?.name || '',
         connectionType: deviceType?.connectionType || 'BLE',
@@ -39,39 +44,50 @@ function handleAddNewDevice() {
 
     addNewDevice.value = false
     newDeviceName.value = ''
-    updateDeviceTypes.value = deviceTypes.value
+    updateDeviceTypes(deviceTypes.value)
 }
 
 function handleEditDevice(device: Device) {
-    console.log('Edit device:', device);
-    editedDevice.value = device;
+    editDevice.value = { ...device };
+    showEditDeviceForm.value = true;
 }
 
 function handleDeleteDevice(device: Device) {
     const index = deviceType?.devices?.findIndex((d) => d.id === device.id);
     if (index !== undefined && index !== -1 && deviceType && deviceType.devices) {
+        console.log('Delete device:', device);
         deviceType.devices.splice(index, 1);
-        console.log('Device deleted:', device);
+        updateDeviceTypes(deviceTypes.value);
     }
 }
 
-function handleSaveDevice() {
-    editedDevice.value!.name = editDeviceName.value;
+const updateDevice = () => {
+    if (!editDevice.value) return;
 
-    deviceType?.devices?.forEach((device) => {
-        if (device.id === editedDevice.value?.id) {
-            device.name = editedDevice.value?.name || '';
+    const updatedDeviceType: DeviceType = {
+        ...deviceType!,
+        devices: deviceType?.devices?.map((device) => {
+            if (device.id === editDevice.value?.id) {
+                return { ...editDevice.value }
+            }
+            return device
+        })
+    }
+
+    const updatedDeviceTypes: DeviceType[] = deviceTypes.value.map((deviceType) => {
+        if (deviceType.name === updatedDeviceType.name) {
+        return { ...updatedDeviceType }
         }
-    });
-
-    editedDevice.value = null;
-    updateDeviceTypes.value = deviceTypes.value;
-    editDeviceName.value = '';
-    console.log('Device saved:', editedDevice.value);
+        return deviceType
+    })
+    updateDeviceTypes(updatedDeviceTypes)
+    editDevice.value = null
+    showEditDeviceForm.value = false
 }
 
 function handleCancelEdit() {
-    editedDevice.value = null;
+    editDevice.value = null;
+    showEditDeviceForm.value = false;
 }
 
 function handleCancelNewDevice() {
@@ -81,7 +97,7 @@ function handleCancelNewDevice() {
 </script>
 
 <template>
-    <main class="pr-4">
+    <main class="pr-4 pb-8">
         <div class="flex flex-col gap-12" v-if="deviceType">
             <div class="w-fit flex justify-between items-center gap-1">
                 <router-link to="/devices" class="px-2 py-1 rounded-md text-2xl font-semibold hover:bg-accent-300">Configure devices</router-link>
@@ -134,36 +150,30 @@ function handleCancelNewDevice() {
                         v-for="device in deviceType.devices" 
                         :key="device.id" 
                         :device="device" 
-                        @edit-device="handleEditDevice" 
-                        @delete-device="handleDeleteDevice" 
+                        @edit-device="handleEditDevice(device)" 
+                        @delete-device="handleDeleteDevice(device)"
                     />
                 </div>
             </div>
         </div>
-        <div v-if="editedDevice" class="absolute top-0 right-0 w-screen h-screen bg-[#000000] opacity-50"></div>
-        <div v-if="editedDevice" class="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] p-6 rounded-xl bg-primary-100 border-2 border-accent-700">
+        <div v-if="showEditDeviceForm && editDevice" class="absolute top-0 right-0 w-screen h-screen bg-[#000000] opacity-50"></div>
+        <div v-if="showEditDeviceForm && editDevice" class="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] p-6 rounded-xl bg-primary-100 border-2 border-accent-700">
             <h2 class="text-xl font-semibold mb-4">Edit Device</h2>
-            <form @submit.prevent="handleSaveDevice">
-                <div class="mb-4">
-                    <label for="deviceName" class="block text-sm font-medium text-gray-700">Device Name</label>
-                    <input
-                        type="text"
-                        id="deviceName"
-                        v-model="editedDevice.name"
-                        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        required
-                    />
+            <form>
+                <div class="mb-4 flex flex-col gap-1">
+                    <label for="name" class="text-md">Device type name</label>
+                    <base-input-field v-model="editDevice.name" label="Name" name="name" placeholder="" />
                 </div>
-                <div class="flex justify-end gap-4">
-                    <base-button type="button" variant="outline" @click="handleCancelEdit">Cancel</base-button>
-                    <base-button type="submit">Save</base-button>
+                <div class="flex justify-between gap-4">
+                    <base-button variant="outline" @click="handleCancelEdit">Cancel</base-button>
+                    <base-button @click="updateDevice">Update</base-button>
                 </div>
             </form>
         </div>
         <div v-if="addNewDevice" class="absolute top-0 right-0 w-screen h-screen bg-[#000000] opacity-50"></div>
         <div v-if="addNewDevice" class="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] p-6 rounded-xl bg-primary-100 border-2 border-accent-700">
             <h2 class="text-xl font-semibold mb-4">Add New Device</h2>
-            <form @submit.prevent="handleAddNewDevice">
+            <form>
                 <div class="mb-4">
                     <label for="deviceName" class="block text-sm font-medium text-gray-700">Device Name</label>
                     <input
@@ -174,9 +184,9 @@ function handleCancelNewDevice() {
                         required
                     />
                 </div>
-                <div class="flex justify-end gap-4">
+                <div class="flex justify-between gap-4">
                     <base-button type="button" variant="outline" @click="handleCancelNewDevice">Cancel</base-button>
-                    <base-button type="submit">Add new device</base-button>
+                    <base-button type="button" @click="handleAddNewDevice">Add new device</base-button>
                 </div>
             </form>
         </div>
