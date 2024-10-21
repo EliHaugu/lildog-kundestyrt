d180517676d747960c83866b048e7b45b5e2f203
EliHaugu <elin.haugum@hotmail.com>
2024-10-22 00:19:16 +0200

    Formatting...

--- server/server_comm/device_connection/urls.py
+++ server/server_comm/device_connection/urls.py
@@ -4,9 +4,13 @@ from .views import AndroidDeviceConnectionView, SerialDeviceConnectionView
 
 urlpatterns = [
     path(
-        "serial/", SerialDeviceConnectionView.as_view(), name="serial-connection",
+        "serial/",
+        SerialDeviceConnectionView.as_view(),
+        name="serial-connection",
     ),
     path(
-        "android/", AndroidDeviceConnectionView.as_view(), name="android-connection",
+        "android/",
+        AndroidDeviceConnectionView.as_view(),
+        name="android-connection",
     ),
 ]
