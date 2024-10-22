a7381067a8c65793d426b411d258b0411b377676
Haakon Karstensen <haakonkarstesen@dhcp-10-220-229-234.wlan.ntnu.no>
2024-10-22 14:15:02 +0200

    Change channels layer from redis to in-memory

--- server/server_comm/server_comm/settings.py
+++ server/server_comm/server_comm/settings.py
@@ -148,10 +148,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
