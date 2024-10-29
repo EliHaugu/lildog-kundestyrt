f604b9e9cbf066f703f7fbcc7e517f6ed7b45c6c
olavsl <olav.selnes.lorentzen@hotmail.com>
2024-10-29 13:59:30 +0100

    resolve review suggestions

--- client/src/components/common/Modal.vue
+++ client/src/components/common/Modal.vue
@@ -1,13 +1,17 @@
 <template>
-  <div v-if="showModal" class="fixed inset-0 bg-[#000000] opacity-30"></div>
+  <div v-if="showModal" class="fixed inset-0 bg-black-100 opacity-30"></div>
   <form
     v-if="showModal"
     class="absolute left-[50%] top-[50%] z-10 flex w-96 translate-x-[-50%] translate-y-[-50%] transform flex-col gap-6 rounded-xl bg-primary-100 p-4 pt-6 shadow-md"
     @submit="handleSubmit"
+    role="dialog"
+    :aria-labelledby="title ? 'modal-title' : undefined"
+    aria-modal="true"
+    :aria-label="title ? undefined : 'Modal Dialog'"
   >
-    <h1 v-if="title" class="text-xl font-semibold">{{ title }}</h1>
+    <h1 v-if="title" id="modal-title" class="text-xl font-semibold">{{ title }}</h1>
     <div class="flex flex-col gap-3">
-      <slot></slot>
+      <slot />
     </div>
     <div class="flex justify-between gap-4">
       <base-button type="button" @click="$emit('close')" :variant="'outline'">Cancel</base-button>
@@ -19,7 +23,7 @@
 <script setup lang="ts">
 import BaseButton from './BaseButton.vue'
 
-const props = defineProps<{
+defineProps<{
   showModal: boolean
   submitButtonText: string
   title?: string
--- client/src/components/navigation/NavButton.vue
+++ client/src/components/navigation/NavButton.vue
@@ -4,7 +4,7 @@
     :aria-expanded="$route.path.includes(to)"
     class="text-md nav_item__rounded relative flex min-w-max items-center gap-1 rounded-l-xl fill-white-100 py-3 pr-10 text-white-100 hover:cursor-pointer hover:duration-200 aria-expanded:bg-primary-200 aria-expanded:fill-accent-500 aria-expanded:text-accent-900 aria-expanded:duration-0 dark:hover:bg-primary-200 dark:hover:text-white-100 dark:aria-expanded:text-white-100"
   >
-    <slot></slot>
+  <slot />
   </router-link>
 </template>
 
