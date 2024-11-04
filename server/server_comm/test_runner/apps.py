af456890b9bfaed833353bbda3a5f106e6119f1f
EliHaugu <elin.haugum@hotmail.com>
2024-11-04 23:57:42 +0100

    Format test runner

--- server/server_comm/test_runner/apps.py
+++ server/server_comm/test_runner/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TestRunnerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'test_runner'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "test_runner"
--- server/server_comm/test_runner/urls.py
+++ server/server_comm/test_runner/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import RunTestFlow
 
 urlpatterns = [
-    path('run/<int:flow_id>/', RunTestFlow.as_view(), name='run-test-flow'),
-    path('run/', RunTestFlow.as_view(), name='run-all-test-flows'),
+    path("run/<int:flow_id>/", RunTestFlow.as_view(), name="run-test-flow"),
+    path("run/", RunTestFlow.as_view(), name="run-all-test-flows"),
 ]
