a73dd86b8a184cb93312f572d185c6d7a8ab812d
Adele Felicia Ellingsen-Grønningsæther <adelefeliciaeg@gmail.com>
2024-11-07 09:01:14 +0100

    Add 400 errors for bad requests (#103)
    * Add 400 errors for bad requests

--- server/server_comm/data_manager/views.py
+++ server/server_comm/data_manager/views.py
@@ -1,9 +1,11 @@
+from django.forms import ValidationError
 from django_filters.rest_framework import (
     DjangoFilterBackend,
     FilterSet,
     NumberFilter,
 )
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError as DRFValidationError
 
 from .models import Category, Device, Edge, Flow, Node
 from .serializers import (
@@ -15,9 +17,23 @@ from .serializers import (
 )
 
 
+def handle_validation(view_func):
+    def wrapper(*args, **kwargs):
+        try:
+            return view_func(*args, **kwargs)
+        except ValidationError as e:
+            raise DRFValidationError(e.messages)
+
+    return wrapper
+
+
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @handle_validation
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -39,37 +55,69 @@ class DeviceListCreateView(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = DeviceFilter
 
+    @handle_validation
+    def perform_create(self, serializer):
+        serializer.save()
+
 
 class DeviceDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+
+    @handle_validation
+    def perform_update(self, serializer):
+        serializer.save()
 
 
 class NodeListCreateView(generics.ListCreateAPIView):
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
 
+    @handle_validation
+    def perform_create(self, serializer):
+        serializer.save()
+
 
 class NodeDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
+
+    @handle_validation
+    def perform_update(self, serializer):
+        serializer.save()
 
 
 class EdgeListCreateView(generics.ListCreateAPIView):
     queryset = Edge.objects.all()
     serializer_class = EdgeSerializer
 
+    @handle_validation
+    def perform_create(self, serializer):
+        serializer.save()
+
 
 class EdgeDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Edge.objects.all()
     serializer_class = EdgeSerializer
+
+    @handle_validation
+    def perform_update(self, serializer):
+        serializer.save()
 
 
 class FlowListCreateView(generics.ListCreateAPIView):
     queryset = Flow.objects.all()
     serializer_class = FlowSerializer
 
+    @handle_validation
+    def perform_create(self, serializer):
+        serializer.save()
+
 
 class FlowDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Flow.objects.all()
     serializer_class = FlowSerializer
+
+    @handle_validation
+    def perform_update(self, serializer):
+        serializer.save()
